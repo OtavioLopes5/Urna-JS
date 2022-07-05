9155101976a3611cc736cc5fb40f5e5592f69261
Otavio Lopes <60718044+OtavioLopes5@users.noreply.github.com>
2022-07-05 16:27:33 -0300

    all files

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Julio Almeida',
+                partido: 'Tan Tofaz',
+                fotos: [
+                    {url:'img1.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'77222',
+                nome: 'Julia Adimela',
+                partido: 'Tofaz Tan',
+                fotos: [
+                    {url:'img3.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '75',
+                nome: 'Otavio Lopes',
+                partido: 'Web Developer',
+                vice: 'Lucilene Lopes',
+                fotos: [
+                    {url:'img2.png', legenda: 'Prefeito'},
+                    {url:'img4.png', legenda: 'Vice-Prefeita', small: true}
+                ]    
+            },
+            {
+                numero: '84',
+                nome: 'Reginaldo Lopes',
+                partido: 'RLF',
+                vice: 'Tata Barra',
+                fotos: [
+                    {url:'img30.png', legenda: 'Prefeito'},
+                    {url:'img5.png', legenda: 'Vice-Prefeita', small: true}
+                ]    
+            },
+        ]
+    }
+];
